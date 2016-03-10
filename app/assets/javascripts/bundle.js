1c7ade48d28581ff607c0db2d69d69e75bdffb68
Justin Tung <JTung1104@gmail.com>
2016-03-10 13:14:08 -0800

    change circles to angry birds

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -359,9 +359,9 @@
 
 	var DrunkenBird = function (options) {
 	  this.img = new Image();
-	  this.img.src = "images/angry_birds.png";
-	  this.srcX = 20;
-	  this.srcY = 20;
+	  this.img.src = "images/bird_sheet.png";
+	  this.srcX = 0;
+	  this.srcY = 0;
 	  this.pos = options.pos;
 	  this.color = options.color || DrunkenBird.COLOR;
 	  this.strokeColor = options.strokeColor || DrunkenBird.STROKE_COLOR;
@@ -372,21 +372,16 @@
 
 	DrunkenBird.COLOR = "#125688";
 	DrunkenBird.STROKE_COLOR = "#EDEEEE";
-	DrunkenBird.RADIUS = 30;
+	DrunkenBird.RADIUS = 50;
 
 	Util.inherits(DrunkenBird, MovingObject);
 
-	// DrunkenBird.prototype.draw = function (ctx) {
-	//   var frame = Math.floor((this.game.tick / 20)) % 2;
-	//   this.srcX = (frame * 100) + 20;
-	//   this.srcY = (Math.random() <= 0.5 ? 20 : 120);
-	//
-	//   ctx.drawImage(
-	//     this.img,
-	//     this.srcX, this.srcY, 80, 80,
-	//     this.pos[0] - 50, this.pos[1] - 50, 80, 80
-	//   );
-	// };
+	DrunkenBird.prototype.draw = function (ctx) {
+	  var frame = Math.floor(this.game.tick / 10) % 3;
+	  this.srcY = frame * 100;
+
+	  ctx.drawImage(this.img, this.srcX, this.srcY, 100, 100, this.pos[0] - 50, this.pos[1] - 50, 100, 100);
+	};
 
 	module.exports = DrunkenBird;
 
